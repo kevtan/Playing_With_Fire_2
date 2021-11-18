53e321d1583ce53b589975168834851087af59b6
Kevin Tan <tankevin@stanford.edu>
2021-11-17 21:43:05 -0800

    Create constants file

--- Playing_With_Fire_2.ino
+++ Playing_With_Fire_2.ino
@@ -2,24 +2,13 @@
 // TODO: Have a game startup sequence to introduce the game
 // TODO: Have a game startup menu to select whether you are playing single player or double player.
 // TODO: Give players "health" by dimming the player LED each time they get hit.
+// TODO: Create a game state class that encapsulates entire all game state variables.
 // Testing - dylan
 #include "math.h"
 #include "actions.h"
 #include "players.h"
+#include "constants.h"
 #include "directions.h"
-
-#define N 8                 // The dimensions of the LED array are N x N.
-#define T 50                // How long (in µs) each row of the LED array is lit every cycle.
-#define REFRESH_PERIOD 10   // How often the scene should be refreshed (in µs).
-#define BOMB_LIFE 4000      // How many frames a bomb with stick around.
-#define BOMB_INTERVAL 100   // How many frames a bomb will stay on or off.
-#define BOMB_BLAST_LIFE 500 // How many frames a bomb's plast animation will play.
-#define ACTION_INTERVAL 250
-#define NULL_REGION_RADIUS 256
-
-#define VRX A5
-#define VRY A4
-#define SW 1
 
 const byte ROWS[N] = {13, 12, 11, 10, 9, 8, 7, 6};
 const byte COLS[N] = {A3, A2, A1, A0, 5, 4, 3, 2};
---
+++ constants.h
@@ -0,0 +1,12 @@
+#define N 8                 // The dimensions of the LED array are N x N.
+#define T 50                // How long (in µs) each row of the LED array is lit every cycle.
+#define REFRESH_PERIOD 10   // How often the scene should be refreshed (in µs).
+#define BOMB_LIFE 4000      // How many frames a bomb with stick around.
+#define BOMB_INTERVAL 100   // How many frames a bomb will stay on or off.
+#define BOMB_BLAST_LIFE 500 // How many frames a bomb's plast animation will play.
+#define ACTION_INTERVAL 250
+#define NULL_REGION_RADIUS 256
+
+#define VRX A5
+#define VRY A4
+#define SW 1
